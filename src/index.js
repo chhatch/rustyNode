ad1ee509d2dd1e4e2bf1aa599b30177d199c921f
Chris Hatch <chris.hatch@hotelengine.com>
2022-05-21 09:53:04 -0500

    A bit of organization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const rules = JSON.stringify(readFileSync("rules.json", "utf8"));
+const rules = JSON.stringify(readFileSync("../rules.json", "utf8"));
 writeFileSync(
   "node_rust/src/main.rs",
   `fn main() {

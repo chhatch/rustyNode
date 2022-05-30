c98cb0f3ccba97bd12a71a49c5df1ccf9df6ee76
Chris Hatch <chris.hatch@hotelengine.com>
2022-05-30 17:21:48 -0500

    Use command alias

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,21 +6,25 @@ export function processArgs() {
     .usage('Usage: $0 [options]')
     .options({
       rustPath: {
+        alias: 'x',
         type: 'string',
         default: 'node_rust/src/main.rs',
         describe: 'Path to write rust to'
       },
       rulesPath: {
+        alias: 'r',
         type: 'string',
         default: 'rules.json',
         describe: 'Path to read rules from'
       },
       inputPath: {
+        alias: 'i',
         type: 'string',
         default: 'input.json',
         describe: 'Path to read input from'
       },
       outputPath: {
+        alias: 'o',
         type: 'string',
         default: 'output.json',
         describe: 'Path to write ouput to'

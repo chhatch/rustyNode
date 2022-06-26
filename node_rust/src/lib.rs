2262f2b426233374466984d59967021a6d40bca7
Chris Hatch <chris.hatch@hotelengine.com>
2022-06-25 20:52:59 -0500

    Finish script update
    To get things working now I had to manually add an index.js to
    node_rust/pkg

--- node_rust/src/lib.rs
+++ node_rust/src/lib.rs
@@ -2,6 +2,9 @@ extern crate serde_json;
 mod rules;
 use std::fs;
 use std::io::Read;
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 fn main() {
     let mut file = fs::File::open("input.json").unwrap();
@@ -12,4 +15,10 @@ fn main() {
     let processed_data_string = rules::run(data_string);
     println!("{}", processed_data_string);
     fs::write("output.json", processed_data_string).expect("Unable to write file");
+}
+
+#[wasm_bindgen]
+pub fn wasm_rules(data_string: String) -> String {
+    console::log_1(&"Greetins from Rustlandia!".into());
+    return rules::run(data_string);
 }
--- src/wasmTest.ts
+++ src/wasmTest.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs'
-import { main } from 'node_rust'
+import { wasm_rules } from 'node_rust'
 
 const dataString = readFileSync('input.json', 'utf8')
 console.log('Here we go!', dataString)
-console.log('Success!', main(dataString))
+console.log('Success!', wasm_rules(dataString))

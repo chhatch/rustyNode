6531080afbd4b66c3be305dcdd0fcc785af1b1d2
Chris Hatch <chris.hatch@hotelengine.com>
2022-06-25 21:18:44 -0500

    test updates

--- src/wasmRules.ts
+++ src/wasmRules.ts
@@ -2,5 +2,4 @@ import { readFileSync } from 'fs'
 import { wasm_rules } from 'node_rust'
 
 const dataString = readFileSync('input.json', 'utf8')
-console.log('Here we go!', dataString)
 console.log('Success!', wasm_rules(dataString))
